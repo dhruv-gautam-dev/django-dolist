6b0bea92394b7254b130e971e2fae25ea126a611
Dhruv <dhruv.gautam.0022@gmail.com>
2026-01-08 13:25:35 +0530

    FEAT: add the search and other columns in the admin dashboard

--- todo/admin.py
+++ todo/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import Task
 
+
+# below is to show the extra column in task table in admin panel
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("task", "is_completed", "created_at", "updated_at")
+    search_fields = ("task",)
+
+
 # Register your models here.
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
 
 
 # first we have creted a model inmodels.py
