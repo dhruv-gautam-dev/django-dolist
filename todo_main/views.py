002eef9a70e140e78b05c95ad93a3ef3d262756a
Dhruv <dhruv.gautam.0022@gmail.com>
2026-01-12 11:12:23 +0530

    FEAT: Apply the pagination

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from todo.models import Task
+from django.core.paginator import Paginator
 
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by("-updated_at")
     completed_tasks = Task.objects.filter(is_completed=True).order_by("-updated_at")
     unCompleted_tasks = Task.objects.filter(is_completed=False).order_by("-updated_at")
+
+    paginator = Paginator(tasks, 5)
+    page = request.GET.get("pg")
+    tasks = paginator.get_page(page)
+    # print("tasksnumberis" tas)
 
     context = {
         "tasks": tasks,
