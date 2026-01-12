da40553aa03c4f8048d778ad539a28bd4f61cef6
Dhruv <dhruv.gautam.0022@gmail.com>
2026-01-12 11:41:01 +0530

    FEAT: refactor template structure

--- todo_main/urls.py
+++ todo_main/urls.py
@@ -22,7 +22,7 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("", views.home, name="home"),
+    path("", views.home, name="home"),
     # To-Do List backend URLs
     path("todo/", include("todo.urls")),
 ]
